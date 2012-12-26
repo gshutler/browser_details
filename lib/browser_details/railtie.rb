0c7fd3f5133814b2ce48e4db35d989bc9cd76423
Garry Shutler <garry@robustsoftware.co.uk>
2012-12-26 21:54:14 +0000

    Documentation and check for AJAX request
    Changed the `log_message` method to be defined when checking for Hatchet and
    Rails to avoid the case statement for every call.
    
    Changed to check the `#xhr?` method on the request to say that JS is enabled on
    top of the previous checks.

--- lib/browser_details/railtie.rb
+++ lib/browser_details/railtie.rb
@@ -1,5 +1,8 @@
 class BrowserDetails
 
+  # Public: Railtie for initializing the BrowserDetails middleware and making
+  # the browser_details.js file available.
+  #
   class Railtie < Rails::Railtie
     initializer "browser_details.insert_middleware" do |app|
       app.config.middleware.use BrowserDetails
--- lib/browser_details/version.rb
+++ lib/browser_details/version.rb
@@ -1,3 +1,5 @@
 class BrowserDetails
-  VERSION = "0.0.2"
+
+  VERSION = "0.0.3"
+
 end
